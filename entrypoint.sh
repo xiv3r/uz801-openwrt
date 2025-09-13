b3b96bf2f3a89c9c6ced81fa8a06b879c8e44cd7
Miguel Fuertes <mjfuertesf@gmail.com>
2025-09-13 08:53:03 +0200

    changes

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-chown -R builder:bilder /home/builder/openwrt/dl \
-                    /home/builder/openwrt/bin \
-                    /home/builder/openwrt/build_dir
-
-exec "$@"
