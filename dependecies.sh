7b9eade83ddcb8a33200c98c42d1e13a7d75fc1c
Miguel Fuertes <mjfuertesf@gmail.com>
2025-09-15 13:35:42 +0200

    changes

---
+++ dependecies.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update && apt-get install -y --no-install-recommends \
+    xz-utils \
+    asciidoc \
+    bash \
+    bc \
+    binutils \
+    bzip2 \
+    flex \
+    git \
+    g++ \
+    gcc \
+    time \
+    util-linux \
+    gawk \
+    gzip \
+    help2man \
+    intltool \
+    libelf-dev \
+    zlib1g-dev \
+    make \
+    libncurses5-dev \
+    libssl-dev \
+    patch \
+    perl \
+    perl-modules \
+    python3-dev \
+    python3-setuptools \
+    rsync \
+    swig \
+    tar \
+    unzip \
+    wget \
+    which \
+    gettext \
+    xsltproc \
+    sudo \
+    android-sdk-libsparse-utils \
+    mkbootimg \
+    zstd \
+    e2fsprogs \
+    && rm -rf /var/lib/apt/lists/*
