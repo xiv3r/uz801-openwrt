af4a8dab8a6116659786bab4b3e4b126d16fa209
Miguel Fuertes <mjfuertesf@gmail.com>
2025-10-04 12:31:26 +0200

    working

--- msm89xx/image/generate_gpt.sh
+++ msm89xx/image/generate_gpt.sh
@@ -1,20 +1,16 @@
 #!/bin/sh -e
-# Usage: generate-gpt.sh <OUTFILE>
 
-OUTFILE="$1"
-TMPDIR="$(mktemp -d)"
-IMG="${TMPDIR}/gpt.img"
-SECTOR_SIZE=512
-SECTORS_TOTAL=7634944
-truncate -s $((SECTORS_TOTAL*SECTOR_SIZE)) "${IMG}"
+OUTFILE=${1:-gpt_both0.bin}
+TMPDIR=$(mktemp -d)
 
-sfdisk "${IMG}" < gpt.table
+# create GPT
+truncate -s 179323904 ${TMPDIR}/gpt.img
 
-{
-  dd if="${IMG}" bs=${SECTOR_SIZE} count=34 status=none
-  dd if="${IMG}" bs=${SECTOR_SIZE} skip=$((SECTORS_TOTAL - 33)) count=33 status=none
-} > "${OUTFILE}"
+cat ./gpt_ext4.table | sfdisk ${TMPDIR}/gpt.img
 
-echo "Wrote: ${OUTFILE}"
-
-rm -rf "${TMPDIR}"
+# create fastboot compatible partition image
+# primary gpt
+dd if=${TMPDIR}/gpt.img of="$OUTFILE" bs=512 count=34
+# backup gpt
+dd if=${TMPDIR}/gpt.img bs=512 skip=2 count=32 >> "$OUTFILE"
+dd if=${TMPDIR}/gpt.img bs=512 skip=350241 >> "$OUTFILE"
