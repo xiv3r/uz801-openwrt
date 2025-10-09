5c8482fe1c2092b9faf96fd6d72424a6389bfa78
Miguel Fuertes <mjfuertesf@gmail.com>
2025-10-09 22:54:58 +0200

    dts from openstick

--- msm89xx/image/msm8916.mk
+++ msm89xx/image/msm8916.mk
@@ -35,7 +35,7 @@ define Device/yiming-uz801v3
   FILESYSTEMS := squashfs
   DEVICE_PACKAGES := uz801-tweaks wpad-basic-wolfssl rmtfs uci-usb-gadget \
                      block-mount f2fs-tools prepare-rootfs-data \
-                     msm8916-wcnss-firmware msm8916-uz801-wcnss-nv msm8916-modem-firmware
+                     msm8916-uz801-wcnss-firmware msm8916-uz801-wcnss-nv msm8916-uz801-modem-firmware
 endef
 TARGET_DEVICES += yiming-uz801v3
 
@@ -44,9 +44,10 @@ define Device/generic-sp970
   DEVICE_VENDOR := Generic
   DEVICE_MODEL := SP970
   FILESYSTEMS := squashfs
+  DEVICE_DTS_DIR:=../dts
   DEVICE_PACKAGES := uz801-tweaks wpad-basic-wolfssl rmtfs uci-usb-gadget \
                      block-mount f2fs-tools prepare-rootfs-data \
-                     msm8916-wcnss-firmware msm8916-sp970-wcnss-nv msm8916-modem-firmware
+                     msm8916-sp970-wcnss-firmware msm8916-sp970-wcnss-nv msm8916-sp970-modem-firmware
 endef
 TARGET_DEVICES += generic-sp970
 
