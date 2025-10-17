be3450d7d49bcf08a9f049179169adee1770412a
Miguel Fuertes <mjfuertesf@gmail.com>
2025-10-17 23:48:12 +0200

    changes, should work!

--- packages/router-display/files/display-manager.sh
+++ packages/router-display/files/display-manager.sh
@@ -1,56 +1,50 @@
 #!/bin/sh
 # /usr/sbin/display-manager
-# Central display timer daemon using FIFO with UCI configuration
 
-# Load UCI config
 . /lib/functions.sh
-
-# UCI config section: system.display
 config_load system
 
-# Default values
+# Defaults
 DEFAULT_TIMEOUT_DIM=5
 DEFAULT_TIMEOUT_OFF=3
 DEFAULT_BRIGHTNESS_DIM_DIVISOR=8
-DEFAULT_BRIGHTNESS_OFF=1
-DEFAULT_BACKLIGHT_PATH="/sys/class/backlight/backlight"
+# FIFO stored in uci as a global shared memory
+# This variable is meant to be used as RO
 DEFAULT_FIFO="/var/run/display.fifo"
-DEFAULT_ENABLE_LOCKSCREEN=0
+DEFAULT_ENABLE_LOCKSCREEN=1
 
-# Load config from UCI (with defaults)
+# Load from UCI
 config_get TIMEOUT_DIM display timeout_dim "$DEFAULT_TIMEOUT_DIM"
 config_get TIMEOUT_OFF display timeout_off "$DEFAULT_TIMEOUT_OFF"
 config_get BRIGHTNESS_DIM_DIVISOR display brightness_dim_divisor "$DEFAULT_BRIGHTNESS_DIM_DIVISOR"
-config_get BRIGHTNESS_OFF display brightness_off "$DEFAULT_BRIGHTNESS_OFF"
-config_get BACKLIGHT_PATH display backlight_path "$DEFAULT_BACKLIGHT_PATH"
 config_get FIFO display fifo "$DEFAULT_FIFO"
 config_get ENABLE_LOCKSCREEN display enable_lockscreen "$DEFAULT_ENABLE_LOCKSCREEN"
 
-# Calculated variables
+# Derived or fixed values
+BRIGHTNESS_OFF=1
 MAX_BRIGHTNESS=$(cat "$BACKLIGHT_PATH/max_brightness" 2>/dev/null || echo 1785)
 BRIGHTNESS_FULL=$MAX_BRIGHTNESS
 BRIGHTNESS_DIM=$((MAX_BRIGHTNESS / BRIGHTNESS_DIM_DIVISOR))
+LOCKSCREEN_FILE="/usr/share/logos/boot_logo.fb"
+POWEROFF_FILE="/usr/share/logos/poweroff_logo.fb"
+BACKLIGHT_PATH="/sys/class/backlight/backlight"
+UPDATE_DISPLAY_BIN="/usr/bin/update-display"
 
-# Timer PIDs
+# PIDs
 TIMER_DIM_PID=""
 TIMER_OFF_PID=""
 
-# Create FIFO if not exists
+# FIFO
 [ -e "$FIFO" ] || mkfifo "$FIFO"
 
-# Cleanup on exit
 trap "rm -f $FIFO; exit 0" EXIT TERM INT
 
-maybe_set_lockscreen() {
-    # Only set lockscreen if enabled in config
-    [ "$ENABLE_LOCKSCREEN" = "1" ] && \
-    [ -n "$1" ] && [ "$1" = "$BRIGHTNESS_DIM" ] && [ -f /etc/boot_logo.fb ] && \
-        cat /etc/boot_logo.fb > /dev/fb0 2>/dev/null
+fb_show_file() {
+    [ -c /dev/fb0 ] && [ -f "$1" ] && cat "$1" > /dev/fb0 2>/dev/null
 }
 
 set_brightness() {
-    echo "$1" > "$BACKLIGHT_PATH/brightness"
-    maybe_set_lockscreen "$1"
+    echo "$1" > "$BACKLIGHT_PATH/brightness" 2>/dev/null
 }
 
 cancel_timers() {
@@ -60,63 +54,61 @@ cancel_timers() {
     TIMER_OFF_PID=""
 }
 
+enqueue() {
+    [ -p "$FIFO" ] && echo "$1" > "$FIFO" 2>/dev/null
+}
+
 start_timers() {
-    # Cancel previous timers
     cancel_timers
-    
-    # Timer 1: DIM after TIMEOUT_DIM seconds
+
     (
         sleep $TIMEOUT_DIM
-        logger -t display "Backlight: DIM"
-        set_brightness $BRIGHTNESS_DIM
-    ) &
-    TIMER_DIM_PID=$!
-    
-    # Timer 2: OFF after (TIMEOUT_DIM + TIMEOUT_OFF) seconds
+        logger -t display "Timer: DIM"
+        enqueue "dim"
+    ) &; TIMER_DIM_PID=$!
+
     TOTAL_TIMEOUT=$((TIMEOUT_DIM + TIMEOUT_OFF))
     (
         sleep $TOTAL_TIMEOUT
-        logger -t display "Backlight: OFF"
-        set_brightness $BRIGHTNESS_OFF
-    ) &
-    TIMER_OFF_PID=$!
+        logger -t display "Timer: OFF"
+        enqueue "off"
+    ) &; TIMER_OFF_PID=$!
 }
 
 logger -t display "Display manager started (dim:${TIMEOUT_DIM}s off:${TIMEOUT_OFF}s)"
 
-# Main loop reading from FIFO
+# Main FIFO loop
 while true; do
     if read -r cmd < "$FIFO"; then
         case "$cmd" in
             full)
                 logger -t display "Backlight: FULL"
                 set_brightness $BRIGHTNESS_FULL
-                
-                # Update display content
-                if [ -x /usr/sbin/update-display ]; then
-                    /usr/sbin/update-display &
-                fi
-                
-                # Start auto-dim/off timers
                 start_timers
+                enqueue "update"
                 ;;
             dim)
-                logger -t display "Backlight: DIM (manual)"
-                cancel_timers
+                logger -t display "Backlight: DIM (enqueue lockscreen)"
                 set_brightness $BRIGHTNESS_DIM
+                [ "$ENABLE_LOCKSCREEN" = "1" ] && enqueue "lockscreen"
                 ;;
             off)
-                logger -t display "Backlight: OFF (manual)"
+                logger -t display "Backlight: OFF"
                 cancel_timers
                 set_brightness $BRIGHTNESS_OFF
                 ;;
-            cancel)
-                logger -t display "Timers cancelled"
-                cancel_timers
+            lockscreen)
+                logger -t display "Show lockscreen"
+                fb_show_file "$LOCKSCREEN_FILE"
+                ;;
+            update)
+                logger -t display "Update display"
+                [ -x $UPDATE_DISPLAY_BIN ] &&  $UPDATE_DISPLAY_BIN
                 ;;
             shutdown)
-                logger -t display "Shutdown signal received"
+                logger -t display "Shutdown sequence"
                 cancel_timers
+                fb_show_file "$POWEROFF_FILE"
                 echo 0 > "$BACKLIGHT_PATH/brightness" 2>/dev/null
                 echo 4 > /sys/class/graphics/fb0/blank 2>/dev/null
                 exit 0
